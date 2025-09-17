7c3929a379f5b4fe81bb9e3cc7685611164a1249
Sarvar8 <sarvar.urinboev8@gmail.com>
2025-09-17 16:27:05 +0900

    feat: Integration of harid ejs file

---
+++ server.js
@@ -0,0 +1,32 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Krish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); 
+
+// 2 Session code
+
+// 3 Views code (Backend da frontend ni yasash)
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({test:"success"});
+});
+
+app.get("/", function(req, res) {
+    res.render("harid");
+})
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
